71db7f14992c3c336aa33c9ebd9405eb41e40556
Ksenia <Ksenia@DESKTOP-FPNQ7IM>
2017-12-09 14:03:54 +0200

    Select Journal in DataGridView1

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -29,11 +29,86 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.substitutionsDataSet = new WindowsFormsApp1.SubstitutionsDataSet();
+            this.journalBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.journalTableAdapter = new WindowsFormsApp1.SubstitutionsDataSetTableAdapters.JournalTableAdapter();
+            this.tableAdapterManager = new WindowsFormsApp1.SubstitutionsDataSetTableAdapters.TableAdapterManager();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.substitutionsDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.journalBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // substitutionsDataSet
+            // 
+            this.substitutionsDataSet.DataSetName = "SubstitutionsDataSet";
+            this.substitutionsDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // journalBindingSource
+            // 
+            this.journalBindingSource.DataMember = "Journal";
+            this.journalBindingSource.DataSource = this.substitutionsDataSet;
+            // 
+            // journalTableAdapter
+            // 
+            this.journalTableAdapter.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.CauseTableAdapter = null;
+            this.tableAdapterManager.ClassifierTableAdapter = null;
+            this.tableAdapterManager.ClassTableAdapter = null;
+            this.tableAdapterManager.DisciplineTableAdapter = null;
+            this.tableAdapterManager.JournalTableAdapter = this.journalTableAdapter;
+            this.tableAdapterManager.ModeTableAdapter = null;
+            this.tableAdapterManager.TeacherTableAdapter = null;
+            this.tableAdapterManager.UpdateOrder = WindowsFormsApp1.SubstitutionsDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(16, 51);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(901, 150);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1069, 303);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.substitutionsDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.journalBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private SubstitutionsDataSet substitutionsDataSet;
+        private System.Windows.Forms.BindingSource journalBindingSource;
+        private SubstitutionsDataSetTableAdapters.JournalTableAdapter journalTableAdapter;
+        private SubstitutionsDataSetTableAdapters.TableAdapterManager tableAdapterManager;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Diagnostics;
 
 namespace WindowsFormsApp1
 {
@@ -15,6 +17,43 @@ namespace WindowsFormsApp1
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void journalBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.journalBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.substitutionsDataSet);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "substitutionsDataSet.Journal". При необходимости она может быть перемещена или удалена.
+            this.journalTableAdapter.Fill(this.substitutionsDataSet.Journal);
+
+        }
+
+        private void journalDataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection("server=desktop-fpnq7im\\sqlexpress;" +
+                                       "Integrated Security=SSPI; "
+                                       + "database=Substitutions;");
+            connection.Open();
+            
+
+            SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM Journal", connection);
+            SqlCommandBuilder cb = new SqlCommandBuilder(da);
+            DataSet ds = new DataSet();
+            da.Fill(ds, "Journal");
+
+            dataGridView1.DataSource = ds.Tables[0];
+            MessageBox.Show("+");
         }
     }
 }
